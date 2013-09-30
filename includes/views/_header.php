fbbba1e61ae64c01b7044072e7c5d7788d320490
jmelosegui <elosegui@gmail.com>
2013-09-30 21:31:47 +0200

    using "protocol-relative URL" on external components
    http://blog.wikimedia.org/2011/07/19/protocol-relative-urls-enabled-on-test-wikipedia-org/

--- includes/views/_header.php
+++ includes/views/_header.php
@@ -6,10 +6,10 @@
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1" /> 
 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
+	<link rel="stylesheet" href="//code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
     <link rel="stylesheet" href="assets/css/styles.css" />
-	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-	<script type="text/javascript" src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
 
 </head> 
 <body> 
